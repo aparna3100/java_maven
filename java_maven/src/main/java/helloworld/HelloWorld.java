33e240c708fd0f390e592b2246feea88b89cee17
user <user@DESKTOP-OQVA7BB>
2023-05-08 09:14:53 +0530

    first commit

---
+++ java_maven/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) { //entry point
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
